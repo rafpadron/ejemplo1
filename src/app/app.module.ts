ddaa75c63f2e191997e5d13d8c81290eca4183fa
Rafa <rafaelpadron0212@gmail.com>
2018-03-01 21:10:16 -0400

    arreglos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ColoresComponent } from './colores/colores.component';
 import { ChaoComponent } from './chao/chao.component';
@@ -14,7 +14,9 @@ import { ChaoComponent } from './chao/chao.component';
     ChaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
